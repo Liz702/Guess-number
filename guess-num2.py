a496032f8e236e8f7a9e6c2c65900bd6f98c5a6a
Liz Lee <liz381226@gmail.com>
2021-02-08 18:12:32 +0800

    Change guess-num1

---
+++ guess-num2.py
@@ -0,0 +1,15 @@
+import random
+r = random.randint(1,100)
+i = 0
+while True:
+    num = input('请输入您猜测的数字： ')
+    num = int(num)
+    i = i + 1
+    print('您猜了', i,'次')
+    if num == r:
+    	print('您猜对了，恭喜')
+    	break
+    elif num > r:
+    	print('比结果大')
+    else:
+    	print('比结果小')
